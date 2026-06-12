f4f36f6e2b0c45e155233a6b20a45f1b6b50c277
yangjian-lang <yangjian-lang@users.noreply.github.com>
2026-06-13 01:22:21 +0800

    fix: register Element Plus icons globally

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import '@/styles/index.scss'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import '@/router/premisstion'
 import CategoryList from '@/components/CategoryList.vue'
@@ -19,5 +20,9 @@ app.use(router)
 app.component('CategoryList', CategoryList)
 app.component('Search', Search)
 app.use(ElementPlus, { size: 'small', zIndex: 3000 })
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.mount('#app')
